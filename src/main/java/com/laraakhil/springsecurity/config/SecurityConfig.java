ab28b152dcccbd36599905654c56700d654f8666
Akhil <24322269+LaraAkhil@users.noreply.github.com>
2020-03-04 21:01:47 +0530

    using query's to get data from user defined schema

--- src/main/java/com/laraakhil/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/laraakhil/springsecurity/config/SecurityConfig.java
@@ -22,7 +22,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		auth.jdbcAuthentication().dataSource(dataSource);
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("select username,password,enabled from users where username = ?")
+				.authoritiesByUsernameQuery("select username,authority from authorities where username = ?");
 	}
 
 	@SuppressWarnings("deprecation")
