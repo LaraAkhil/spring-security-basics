27701ef7a32a298d09852af2726a873023479a98
Akhil <24322269+LaraAkhil@users.noreply.github.com>
2020-03-10 18:38:19 +0530

    configure oauth w/ github&google

--- src/main/java/com/laraakhil/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/laraakhil/springsecurity/config/SecurityConfig.java
@@ -1,38 +1,29 @@
 package com.laraakhil.springsecurity.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
-@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-		auth.inMemoryAuthentication().withUser("user").password("user").roles("user").and().withUser("root")
-				.password("root").roles("admin");
-
-	}
-
-	@SuppressWarnings("deprecation")
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-
-		return NoOpPasswordEncoder.getInstance();
-
-	}
-
-	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/admin").hasRole("admin").antMatchers("/user").hasAnyRole("user", "admin")
-				.antMatchers("/").permitAll().and().formLogin();
+
+		SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler("/");
+
+		http.authorizeRequests().antMatchers("/", "/error", "/webjars/**").permitAll().anyRequest().authenticated()
+				.and().csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+				.logout(l -> l.logoutSuccessUrl("/").permitAll())
+				.exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+				.oauth2Login(o -> o.failureHandler((request, response, exception) -> {
+					request.getSession().setAttribute("error.message", exception.getMessage());
+					handler.onAuthenticationFailure(request, response, exception);
+				}));
 	}
 
 }
--- src/main/java/com/laraakhil/springsecurity/controllers/HomeController.java
+++ src/main/java/com/laraakhil/springsecurity/controllers/HomeController.java
@@ -1,26 +1,16 @@
 package com.laraakhil.springsecurity.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping()
 public class HomeController {
 
 	@GetMapping({ "", "/" })
 	public String homeRoute() {
-		return ("<h2>Welcome<h2>");
-	}
-
-	@GetMapping({ "/user" })
-	public String userRoute() {
-		return ("<h2>Welcome user<h2>");
-	}
-
-	@GetMapping({ "/admin" })
-	public String adminRoute() {
-		return ("<h2>Welcome Admin<h2>");
+		return "index.html";
 	}
 
 }
